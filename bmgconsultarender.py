222ce45737ec3fcc032dde46fb86fe6c1f53d9b0
vitorpaganucci <131719785+vitorpaganucci@users.noreply.github.com>
2023-05-21 14:09:14 -0300

    Update bmgconsultarender.py

--- bmgconsultarender.py
+++ bmgconsultarender.py
@@ -24,64 +24,68 @@ contatos_df = pd.read_excel("bmgconsulta.xlsx")
 
 
 for i, cpf2 in enumerate(contatos_df['cpf']):
-    cpf2 = contatos_df.loc[i, "cpf"]
+    try:
+        cpf2 = contatos_df.loc[i, "cpf"]
 
-    username = 'vitor.helpvix'
-    password = '70da6fe%'
-    codigoEntidade = 1581
+        username = 'vitor.helpvix'
+        password = '70da6fe%'
+        codigoEntidade = 1581
 
-    client = Client('https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl')
+        client = Client('https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl')
 
-    entrada = {
-      'login': username,
-      'senha': password,
-      'codigoEntidade': codigoEntidade,
-      'cpf': cpf2,
+        entrada = {
+          'login': username,
+          'senha': password,
+          'codigoEntidade': codigoEntidade,
+          'cpf': cpf2,
 
 
-    }
-    # Aqui você pode chamar as operações disponíveis no Web Service
-    result = client.service.buscarCartoesDisponiveis(entrada)
+        }
+        # Aqui você pode chamar as operações disponíveis no Web Service
+        result = client.service.buscarCartoesDisponiveis(entrada)
 
-    # Imprimir o resultado da operação
-    #print(result)
-    cartoesRetorno = result.cartoesRetorno
-    #numeroContaInterna = cartoesRetorno.numeroContaInterna
-    #print(cartoesRetorno)
-    numeroContaInterna = cartoesRetorno[0]['numeroContaInterna']
-    for i in range(3):
-        try:
+        # Imprimir o resultado da operação
+        #print(result)
+        cartoesRetorno = result.cartoesRetorno
+        #numeroContaInterna = cartoesRetorno.numeroContaInterna
+        #print(cartoesRetorno)
+        numeroContaInterna = cartoesRetorno[0]['numeroContaInterna']
+        for i in range(3):
+            try:
+                numeroContaInterna = cartoesRetorno[i]['numeroContaInterna']
+                #print(numeroContaInterna)
+
+            except:
+                break
+        #print(i)
+
+            #print(cpf2, 'cliente possui dois beneficios')
+        for i in range(2):
             numeroContaInterna = cartoesRetorno[i]['numeroContaInterna']
-            #print(numeroContaInterna)
+            with client.settings(strict=False):
+                entrada = {
+                    'login': username,
+                    'senha': password,
+                    'codigoEntidade': '1581',
+                    'cpf': cpf2,
+                    'numeroContaInterna': numeroContaInterna,
+                    'tipoSaque': 1,
+                    'cpfImpedidoComissionar': False
+                }
+                result = client.service.buscarLimiteSaque(entrada)
+                #print(result)
 
-        except:
-            break
-    #print(i)
-
-        #print(cpf2, 'cliente possui dois beneficios')
-    for i in range(2):
-        numeroContaInterna = cartoesRetorno[i]['numeroContaInterna']
-        with client.settings(strict=False):
-            entrada = {
-                'login': username,
-                'senha': password,
-                'codigoEntidade': '1581',
-                'cpf': cpf2,
-                'numeroContaInterna': numeroContaInterna,
-                'tipoSaque': 1,
-                'cpfImpedidoComissionar': False
-            }
-            result = client.service.buscarLimiteSaque(entrada)
-            #print(result)
-
-            valorSaqueMaximo = result.valorSaqueMaximo
-            valorSaqueMaximo = str(valorSaqueMaximo).replace(".", ",")
-#             if valorSaqueMaximo == 0:
-#                 pass
-#             else:
-            print(cpf2, valorSaqueMaximo)
-            planilha_ativa.append([cpf2, valorSaqueMaximo])
-
+                valorSaqueMaximo = result.valorSaqueMaximo
+                valorSaqueMaximo = str(valorSaqueMaximo).replace(".", ",")
+    #             if valorSaqueMaximo == 0:
+    #                 pass
+    #             else:
+                print(cpf2, valorSaqueMaximo)
+                planilha_ativa.append([cpf2, valorSaqueMaximo])
+    
+    except:
+        print('erro')
+        pass
 
 
 
