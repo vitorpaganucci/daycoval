8d4ccc7dba49575c0f6bb42e0880e92abbc22a32
vitorpaganucci <131719785+vitorpaganucci@users.noreply.github.com>
2023-06-23 12:06:50 -0300

    Update bmgconsultarender.py

--- bmgconsultarender.py
+++ bmgconsultarender.py
@@ -6,7 +6,18 @@ logging.getLogger('zeep.wsdl.bindings.soap').setLevel(logging.ERROR)
 
 
 import pandas as pd
-contatos_df = pd.read_excel("bmgconsulta.xlsx")
+
+headers = CaseInsensitiveDict()
+headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36"
+headers["Accept"] = "application/json"
+headers["apikey"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwa3RnaXd6d2xtYWFha3l3aGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM5MjAwNTgsImV4cCI6MTk5OTQ5NjA1OH0.BvmnwvNUcAnCXJTocXlX6kcSL44l5bgY4MGUFdEIKyw"
+headers["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwa3RnaXd6d2xtYWFha3l3aGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM5MjAwNTgsImV4cCI6MTk5OTQ5NjA1OH0.BvmnwvNUcAnCXJTocXlX6kcSL44l5bgY4MGUFdEIKyw"
+url = 'https://qpktgiwzwlmaaakywheh.supabase.co/rest/v1/saque'
+from zeep import Client
+import logging
+logging.getLogger('zeep.wsdl.bindings.soap').setLevel(logging.ERROR)
+
+contatos_df = pd.read_excel("bmgconsulta2.xlsx")
 
 
 
@@ -14,8 +25,6 @@ contatos_df = pd.read_excel("bmgconsulta.xlsx")
 for i, cpf in enumerate(contatos_df['cpf']):
     try:
         cpf2 = str(contatos_df.loc[i, "cpf"])
-
-        
         if '-' or '.' in cpf2:
             cpf2 = cpf2.replace("-","" ).replace(".","" )
         else:
@@ -68,18 +77,6 @@ for i, cpf in enumerate(contatos_df['cpf']):
                     limiteDisponivel = result.limiteDisponivel
                     limiteDisponivel = str(limiteDisponivel).replace(".", ",")
                     print(cpf2, limiteDisponivel, 'RMC')
-                    
-                    headers = CaseInsensitiveDict()
-                    headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36"
-                    headers["Accept"] = "application/json"
-                    headers["apikey"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwa3RnaXd6d2xtYWFha3l3aGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM5MjAwNTgsImV4cCI6MTk5OTQ5NjA1OH0.BvmnwvNUcAnCXJTocXlX6kcSL44l5bgY4MGUFdEIKyw"
-                    headers["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwa3RnaXd6d2xtYWFha3l3aGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM5MjAwNTgsImV4cCI6MTk5OTQ5NjA1OH0.BvmnwvNUcAnCXJTocXlX6kcSL44l5bgY4MGUFdEIKyw"
-
-
-
-
-                    url = 'https://qpktgiwzwlmaaakywheh.supabase.co/rest/v1/saque'
-
                     user_data = {"saldo": limiteDisponivel, "cpf": cpf2}
 
                     response = requests.post(url=url, headers=headers, json=user_data)
